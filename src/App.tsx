25df7bf150aa9f0f446b57ea3ba20ade9b0b9a1e
Felype-Henrique <felypehenrique25@gmail.com>
2022-04-29 10:34:59 -0300

    TableArea

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,32 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
 
-import { getcurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+
+import { TableArea } from "./components/TableArea";
 
 const App = () => {
   const [list, setList] = useState(items);
-  const [filtereredList, setFilteredList] = useState<Item[]>([]);
-  const [currentMonth, setCurrentMonth] = useState(getcurrentMonth());
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
 
   useEffect(() => {
     setFilteredList( filterListByMonth(list, currentMonth) );
-  }, [list, currentMonth])
+  }, [list, currentMonth]);
 
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
-      <C.Body>...</C.Body>
+      <C.Body>
+        {/* Área de informações */}
+
+        {/* Área de inserção */}
+
+        <TableArea list={filteredList} />
+
+      </C.Body>
     </C.Container>
   );
 };
