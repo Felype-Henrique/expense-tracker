ce3645eb3f7f1361034e393f6212da42f452061f
Felype-Henrique <felypehenrique25@gmail.com>
2022-04-29 11:35:19 -0300

    TableItem

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -26,5 +26,7 @@ export const formatDate = (date: Date): string => {
   let month = date.getMonth() + 1;
   let day = date.getDate();
 
-  return `${day}/${month}/${year}`;
+  return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
 }
+
+const addZeroToDate = (n:number): string => n < 10 ? `0${n}` : `${n}`;
