db18c804493cfb753722eee5248997b7e3121755
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-02 16:15:59 -0300

    InputArea

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import * as C from "./App.styles";
 
 import { Item } from "./types/Item";
-import { Category } from "./types/Category";
 
 import { categories } from "./data/categories";
 import { items } from "./data/items";
@@ -12,6 +11,7 @@ import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 
 import { TableArea } from "./components/TableArea";
 import { InfoArea } from "./components/InfoArea";
+import { InputArea } from './components/InputArea';
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -45,6 +45,12 @@ const App = () => {
     setCurrentMonth(newMonth);
   }
 
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -59,7 +65,7 @@ const App = () => {
         expense={expense}
         />
 
-        {/* Área de inserção */}
+        <InputArea onAdd={handleAddItem} />
 
         <TableArea list={filteredList} />
 
